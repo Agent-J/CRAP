cd2c1daedaaa7bd9552ea1761a1af90f2af13eed
Eugene Oborin <mail@agent-j.ru>
2016-11-23 21:32:46 +0300

    Fix timers

--- js/main.js
+++ js/main.js
@@ -10,9 +10,6 @@ var appendToBody = function(text, tag)
 
 document.addEventListener("DOMContentLoaded", function()
 {
-	appendToBody('hello');
-	setTimeout(function()
-	{
-		appendToBody('Don\'t be a crap', 'h3');
-	}, 2000);
+	setTimeout(function(){appendToBody('Hello!')}, 2000);
+	setTimeout(function(){appendToBody('Don\'t be a crap', 'h2')}, 4000);
 });
