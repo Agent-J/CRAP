b299c666494ad05f5b32d23797664942954d26d5
Eugene Oborin <mail@agent-j.ru>
2016-11-25 22:32:47 +0300

    First JS tests

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 'use strist';
 
+/*
+ * Math.hypot polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/hypot
+*/
+Math.hypot = Math.hypot || function()
+{
+	var i, y = 0;
+	
+	for (i = 0; i < arguments.length; i++)
+	{
+		if(arguments[i] === Infinity || arguments[i] === -Infinity)
+		{
+			return Infinity;
+		}
+		y += arguments[i] * arguments[i];
+	}
+	
+	return Math.sqrt(y);
+};
+
+
 $(function()
 {
-	$('.crap-character').animate( {'left':1000}, 5000);
+
 });
