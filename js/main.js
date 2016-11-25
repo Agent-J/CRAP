70453ebe4d586d7ab24453e2be3431904b704b35
Eugene Oborin <mail@agent-j.ru>
2016-11-25 22:37:19 +0300

    Some calibration

--- js/main.js
+++ js/main.js
@@ -23,5 +23,55 @@ Math.hypot = Math.hypot || function()
 
 $(function()
 {
-
+	var playground = $('#playground');
+	var character  = $('#character');
+	
+	$('#playground').click(function(e)
+	{
+		var moveTo = {
+			'top'  : e.pageY - playground.offset().top,
+			'left' : e.pageX - playground.offset().left
+		};
+		
+		moveCharacter(character, moveTo);
+	});
+	
+	var getCharacterShift = function(character, coords, negative=false)
+	{
+		var k = negative ? -1 : 1;
+		coords.top  -= k * (character.height());
+		coords.left -= k * (character.width() / 2);
+		return coords;
+	};
+	
+	var moveCharacter = function(character, moveTo)
+	{
+		setCharacterDirection();
+		var a = moveTo;
+		var b = getCharacterShift(character, character.position(), true);
+		var g = Math.atan2(a.top-b.top, a.left-b.left);
+		var position;
+		if(g < -2.36 || g > 2.36) position = 'left';
+		else if(g < -0.79 )  position = 'up';
+		else if(g < 0.79 )  position = 'right';
+		else  position = 'down';
+		character.addClass('move');
+		character.addClass(position);
+		
+		
+		
+		
+		
+		
+		moveTo = getCharacterShift(character, moveTo);
+		
+		var curPos   = character.position();
+		var distance = Math.hypot(moveTo.left - curPos.left, moveTo.top - curPos.top);
+		
+		character.animate(moveTo, distance*4, 'linear', function(){character.removeClass('move up down left right')});
+	};
+	
+	var setCharacterDirection = function()
+	{
+	};
 });
